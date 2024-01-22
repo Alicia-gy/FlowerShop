8a306b1a968469f466f6f915b05c05f24b7c5cdb
ecs90 <ecs_salas@hotmail.com>
2024-01-23 00:01:33 +0100

    Repo txt created, deleted test class

--- src/main/java/flowershop/domain/Product.java
+++ src/main/java/flowershop/domain/Product.java
@@ -1,4 +1,27 @@
 package flowershop.domain;
 
-public abstract class Product {
+public class Product {
+    private long id;
+    private String nombre;
+
+    public Product(long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
